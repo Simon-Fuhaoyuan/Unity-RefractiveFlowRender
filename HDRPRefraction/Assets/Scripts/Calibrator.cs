991b872afb0f2bfa541db68ff7c34764852def04
Simon-Fuhaoyuan <simon-fuhaoyuan@sjtu.edu.cn>
2021-03-15 13:56:22 +0800

    Calibration

--- HDRPRefraction/Assets/Scripts/Calibrator.cs
+++ HDRPRefraction/Assets/Scripts/Calibrator.cs
@@ -3,51 +3,111 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System;
 
 public class Calibrator : MonoBehaviour
 {
     public Camera mainCamera;
-    public int waitingFrames;
     public GameObject plane;
     public bool isTrain;
-    public int startImageIndex = 0;
 
+    private bool legal;
     private ImageSynthesis imageSynthesis;
     private string materialFormat;
-    private int count;
-    private int waitingCounter;
+    private string baseRoot;
+    private string recordFolder;
+    private string calibrationFolder;
+    private FileInfo[] recordFiles;
+    private int counter;
+    private int numberOfCalibrations;
+    private int calibrationCounter;
+    private string currentImageId;
+    private string currentCalibrationFolder;
     private Recorder recorder;
     private List<GameObject> prefabs;
+    private bool switch_;
 
     void Start()
     {
-        imageSynthesis = mainCamera.GetComponent<ImageSynthesis>();
-        materialFormat = "graycode_512_512/Materials/graycode_";
-        count = 1;
-        waitingCounter = 0;
+        CheckLegality();
+
+        // counter is used to control frame and produce images
+        // I find in HDRP, after running, the very first few frames will be the same
+        // i.e. I will save a few repeating images.
+        // Thus, I change the counter to negative, so that it can avoid rendering same images.
+        counter = -10;
+        calibrationCounter = 0;
+
         recorder = new Recorder();
-        prefabs = new List<GameObject>();
         // recorder.ParseFile("train/recorder/record_10.txt");
         // recorder.Show();
+        materialFormat = "graycode_512_512/Materials/graycode_";
+        prefabs = new List<GameObject>();
+        
+        calibrationFolder = Path.Combine(baseRoot, "calibration");
+        MakeDirectory(calibrationFolder);
+
+        DirectoryInfo recordInfo = new DirectoryInfo(recordFolder);
+        recordFiles = recordInfo.GetFiles();
+        // foreach (FileInfo info in recordFiles)
+        // {
+        //     Debug.Log(info.Name);
+        // }
+        numberOfCalibrations = recordFiles.Length;
+
+        switch_ = false;
+        Application.targetFrameRate = 30;
+        currentImageId = "";
+        currentCalibrationFolder = "";
     }
 
-    void FixedUpdate()
+    void LateUpdate()
     {
-        // if (count > 18)
-        //     count = 1;
-        // if (waitingCounter == 0)
-        // {
-        //     Material graycode = Resources.Load(materialFormat + count.ToString(), typeof(Material)) as Material;
-        //     plane.GetComponent<MeshRenderer>().material = graycode;
-        // }
-        // if (waitingCounter == waitingFrames)
-        // {
-        //     imageSynthesis.Save(string.Format("debug{0}.png", count), 512, 512, "", 0);
-        //     count += 1;
-        //     waitingCounter = -1;
-        // }
-        // waitingCounter += 1;
-        // print(waitingFrames);
+        if (!legal || counter >= numberOfCalibrations)
+        {
+            return;
+        }
+        else if (counter < 0)
+        {
+            counter += 1;
+            return;
+        }
+
+        if (calibrationCounter == 0)
+        {
+            string recordFile = recordFiles[counter].Name;
+            recordFile = Path.Combine(recordFolder, recordFile);
+            ArrangeScene(recordFile);
+            CreateCalibrationSubfolder(recordFile);
+            calibrationCounter += 1;
+            return;
+        }
+        else if (calibrationCounter >= 1 && calibrationCounter <= 18)
+        {
+            if (!switch_)
+            {
+                Material graycode = Resources.Load(materialFormat + calibrationCounter.ToString(), typeof(Material)) as Material;
+                plane.GetComponent<MeshRenderer>().material = graycode;
+            }
+            else
+            {
+                imageSynthesis.Save(
+                    string.Format("{0}.png", calibrationCounter),
+                    512, 512, currentCalibrationFolder, 0
+                    );
+                calibrationCounter += 1;
+            }
+
+            switch_ = !switch_;
+            return;
+        }
+        else
+        {
+            DestroyAllObjects();
+            calibrationCounter = 0;
+            counter += 1;
+            switch_ = false;
+        }
     }
 
     GameObject LoadPrefab(string path)
@@ -77,5 +137,82 @@ public class Calibrator : MonoBehaviour
         {
             info.Create();
         }
+    }
+
+    void CheckLegality()
+    {
+        legal = true;
+
+        // Check camera
+        if (mainCamera == null)
+        {
+            Debug.LogError("The main camera is null!");
+            legal = false;
+        }
+
+        // Check ImageSyntesis
+        imageSynthesis = mainCamera.GetComponent<ImageSynthesis>();
+        if (imageSynthesis == null)
+        {
+            Debug.LogError("Main camera doesn't have component imageSynthesis.");
+            legal = false;
+        }
+
+        // Check base root
+        if (isTrain)
+        {
+            baseRoot = "train";
+        }
+        else
+        {
+            baseRoot = "valid";
+        }
+        recordFolder = Path.Combine(baseRoot, "recorder");
+        DirectoryInfo info = new DirectoryInfo(recordFolder);
+        if (!info.Exists)
+        {
+            Debug.LogError("No record folder, do you finish generating RGB?");
+            legal = false;
+        }
+    }
+
+    void DestroyAllObjects()
+    {
+        foreach (GameObject gameObject in prefabs)
+        {
+            Destroy(gameObject);
+        }
+        prefabs.Clear();
+    }
+
+    void ArrangeScene(string recordFile)
+    {
+        recorder.ParseFile(recordFile);
+        mainCamera.transform.localEulerAngles = recorder.cameraRotation;
+        int numberOfObjects = recorder.numberOfObjects;
+        for (int i = 0; i < numberOfObjects; ++i)
+        {
+            GameObject prefab = LoadPrefab(recorder.prefabPaths[i]);
+            prefab.transform.localPosition = recorder.positions[i];
+            prefab.transform.localEulerAngles = recorder.rotations[i];
+            prefab.transform.localScale = new Vector3(recorder.scales[i], recorder.scales[i], recorder.scales[i]);
+            
+            Material glassMat = LoadGlassMaterial(i + 1, recorder.IORs[i]);
+            MeshRenderer meshRenderer = prefab.GetComponent<MeshRenderer>();
+            int length = meshRenderer.materials.Length;
+            for (int j = 0; j < length; ++j)
+            {
+                meshRenderer.materials[j] = glassMat;
+            }
+
+            prefabs.Add(prefab);
+        }
+    }
+
+    void CreateCalibrationSubfolder(string recordFile)
+    {
+        currentImageId = recordFile.Split(new char[2]{'_', '.'})[1];
+        currentCalibrationFolder = Path.Combine(calibrationFolder, currentImageId);
+        MakeDirectory(currentCalibrationFolder);
     }
 }
